2b9d7c7db9ac0c5ea5a194ae861f5a1242e022bc
Moses Maximino Cosme II <mosesmaxcosme@gmail.com>
2022-06-11 09:25:52 -0500

    style(layout): format

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react"
 
 const Footer = () => {
-    return (
-        <>
-        <footer
-          style={{
-            marginTop: `var(--space-5)`,
-            fontSize: `var(--font-sm)`,
-          }}
-        >
-          © {new Date().getFullYear()} &middot; Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-        </>
-    )
+  return (
+    <>
+      <footer
+        style={{
+          marginTop: `var(--space-5)`,
+          fontSize: `var(--font-sm)`,
+        }}
+      >
+        © {new Date().getFullYear()} &middot; Built with
+        {` `}
+        <a href="https://www.gatsbyjs.com">Gatsby</a>
+      </footer>
+    </>
+  )
 }
 
-export default Footer;
+export default Footer
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./Header"
-import Footer from './Footer'
+import Footer from "./Footer"
 import "./layout.css"
 
 const Layout = ({ children }) => {
