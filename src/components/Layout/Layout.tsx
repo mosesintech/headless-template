e4979f64c195f0c8e406f92bf74f6685f37c177f
Moses Maximino Cosme II <mosesmaxcosme@gmail.com>
2022-06-11 19:56:58 -0500

    feat(templates): add default for wordpress pages

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,41 +1,25 @@
 import * as React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 
+import Seo from "../Seo"
 import Header from "./Header"
 import Footer from "./Footer"
-import "./layout.css"
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: `var(--size-content)`,
-          padding: `var(--size-gutter)`,
-        }}
-      >
-        <main>{children}</main>
-        <Footer />
-      </div>
-    </>
-  )
+interface LayoutProps {
+  children: React.ReactNode
+  title?: string
+  archivePath?: string
+  isCategory?: boolean
 }
 
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+const Layout: React.FC<LayoutProps> = ({ children, title }) => {
+  return (
+    <>
+      <Seo title={title} />
+      <Header />
+      {children}
+      <Footer />
+    </>
+  )
 }
 
 export default Layout
---
+++ src/templates/page/default.tsx
@@ -0,0 +1,49 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import { TemplatePageProps } from "../../interfaces"
+import Layout from "../../components/Layout/Layout"
+// import FlexibleContent from "../../components/FlexibleContent"
+
+const DefaultPageTemplate: React.FC<TemplatePageProps> = props => {
+  const {
+    data: {
+      page: { title, uri, slug, template },
+    },
+  } = props
+
+  return (
+    <>
+      <Layout title={title}>
+        {/* {!!template && (
+          <FlexibleContent
+            modules={template.flexibleContentModules.contentModule}
+            data={{
+              title,
+              uri,
+              slug,
+            }}
+          />
+        )} */}
+        <p>Page</p>
+      </Layout>
+    </>
+  )
+}
+
+export default DefaultPageTemplate
+
+export const FlexibleContentQuery = graphql`
+  query DefaultPage($id: String!) {
+    page: wpPage(id: { eq: $id }) {
+      title
+      slug
+      uri
+    #   template {
+    #     ... on WpDefaultTemplate {
+    #       ...DefaultTemplateFragment
+    #     }
+    #   }
+    }
+  }
+`
